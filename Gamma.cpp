587ab6eebcc1a58fcbf9d889ba017049b21b8059
Shoaib-78637 <syedshoaibabbas786@gmail.com>
2020-10-09 15:41:11 +0500

    new funtion add

--- Gamma.cpp
+++ Gamma.cpp
@@ -17,17 +17,32 @@ void printBar()
 }
 cout<<endl;
 }
+void Print()
+{
+	for (int i=0;i<9;i++)
+	cout<<"*\n";
+	
+	
+	for (int i=0;i<9;i++)
+{	
+	if (i>2 && i<7)
+	cout<<"*";
+	else
+	cout<<"-";
+}
+cout<<"\n";
+}
 
 int main ()
 {
 	printBar();
 	for (int i=0;i<5;i++)
-		cout<<"I love Pakistan."<<endl;	
+		cout<<"I love Pakistan"<<endl;	
 	printBar();
 	bool isTodayFriday;
 	isTodayFriday= true;
 	if(isTodayFriday)
-	cout<<"Bhai Nahana Parey gaa";
+	cout<<"Bhai Nahana Parey gaa aj to tum ko";
 	else
 	cout<<"Khaire hi hia Cheetey";
 }
